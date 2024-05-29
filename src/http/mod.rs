3c23ff875a0073aa96d7d5c8d5b954d5db909fe0
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2024-05-29 21:08:50 +0700

    feat: init response and status_code

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,8 +1,12 @@
 pub mod request;
 pub mod method;
 pub mod query_string;
+pub mod response;
+pub mod status_code;
 
 pub use request::Request;
 pub use request::ParseError;
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
+pub use status_code::StatusCode;
